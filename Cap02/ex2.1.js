90a875c57ebbe37978927b1a8e55abe1f94e2453
PauloRoberto82 <pr.tech82@hotmail.com>
2024-11-17 17:47:32 -0300

    "17/11"

--- Cap02/ex2.1.js
+++ Cap02/ex2.1.js
@@ -7,6 +7,6 @@ const resp2 = document.querySelector('h4');
 frm.addEventListener("submit", (e) => {
  const nome = frm.inNome.value //obtém o nome digitado
  resp.innerText = `Olá, ${nome}!`//exibe a resposta do programa 
- resp2.innerText = `De novo, ${nome}!`//exibe a resposta do programa *a mesma resposta que a anterior, mas em um elemento diferente
+ resp2.innerText = `De novo, ${nome}!`//exibe a mesma resposta que a anterior, mas em um elemento(tag) diferente *Eu que repliquei para testar
  e.preventDefault()//evita o envio do formulário form
 })
---
+++ Cap02/ex2.2.js
@@ -0,0 +1,6 @@
+/* Métodos querySelector() ou getElementById()
+ex:
+const resp = document.querySelector("h3") primeiro elemento h3 da página
+const cor = document.querySelector("#inCor") elemento com id=inCor
+const lista = document.querySelector(".lista") elemento com classe=lista
+*/
