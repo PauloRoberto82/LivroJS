c318db511fe768c6fed4ba76209dc396d757f669
PauloRoberto82 <pr.tech82@hotmail.com>
2024-11-23 13:42:13 -0300

    23/11
    Acrescentei calcular valores em float de 0.01 no input e parsefloat no addeventlistener

--- Cap02/ex2.6.js
+++ Cap02/ex2.6.js
@@ -7,17 +7,13 @@ const pagar = document.querySelector("h3");
 
 //Cria um "ouvinte" de eventom acionado quando o botao submit for clicado
 frm.addEventListener("submit",(e) =>{
-const valor = Number(frm.min15.value)// obtem conteudo dos campos
+const valor = parseFloat(frm.min15.value)// obtem conteudo dos campos
 
 const duracao = Number(frm.tempo.value)
 
 const b15 = Math.ceil(duracao/15)
 
 const valor2 = b15 * valor
-
-
-
-
 
 pagar.innerText = `O Valor a pagar R$ ${valor2}` //exibe as respostas
  
